1a81351d4af601020e459b51317ec063b15a1733
codeesh <vineshpersaud@gmail.com>
2015-11-19 15:45:42 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  x = 1
+  y = 0
+  z = 1
+  sum = 0
+  list =[1,2]
+  even_list = []
+  until limit.to_i <= list[x] 
+    list <<  list[y]+list[z]
+    x += 1
+    y += 1
+    z += 1
+  end
+  list.each do |even| 
+    if even % 2 == 0  && even < limit
+      even_list << even
+    end
+  end
+   return even_list.inject{|sum,a| sum + a }
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+    def initialize(limit)
+    @limit = limit
+
+    end
+
+    def sum
+  x = 1
+  y = 0
+  z = 1
+  sum = 0
+  list =[1,2]
+  even_list = []
+  until @limit.to_i <= list[x] 
+    list <<  list[y]+list[z]
+    x += 1
+    y += 1
+    z += 1
+  end
+  list.each do |even| 
+    if even % 2 == 0  && even < @limit
+      even_list << even
+    end
+  end
+   return even_list.inject{|sum,a| sum + a }
+end
+end
